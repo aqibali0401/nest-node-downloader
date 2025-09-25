4038cfd920abd61f20e0c1a44f4b3d7a6aa6653a
Amrik Singh <amriks@damcogroup.com>
2025-09-25 17:15:14 +0530

    some fixes in dependency injections

--- src/core/recovery/recovery.module.ts
+++ src/core/recovery/recovery.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RecoveryService } from './recovery.service';
+import { ProcessRecoveryService } from './process-recovery.service';
+import { DatabaseModule } from '../database/database.module';
+import { NetworkModule } from '../network/network.module';
 
 /**
  * Recovery module for process recovery and state persistence
  */
 @Module({
-  providers: [RecoveryService],
-  exports: [RecoveryService],
+  imports: [DatabaseModule, NetworkModule],
+  providers: [RecoveryService, ProcessRecoveryService],
+  exports: [RecoveryService, ProcessRecoveryService],
 })
 export class RecoveryModule {}
