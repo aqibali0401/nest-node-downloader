de4ce7bda714ca20fde68873f0ab9cddf6c1aa33
Amrik Singh <amriks@damcogroup.com>
2025-09-25 17:12:16 +0530

    some fixes in dependency injections

--- src/core/crash/crash.module.ts
+++ src/core/crash/crash.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CrashService } from './crash.service';
+import { CrashSimulationService } from './crash-simulation.service';
 
 /**
  * Crash simulation module for testing recovery mechanisms
  */
 @Module({
-  providers: [CrashService],
-  exports: [CrashService],
+  providers: [CrashService, CrashSimulationService],
+  exports: [CrashService, CrashSimulationService],
 })
 export class CrashModule {}
